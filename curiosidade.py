bb6c022816591b18a94b3ea3c5a1675e7d1babcc
andrebahia01 <andre_santana21@hotmail.com>
2025-04-27 23:34:19 -0300

    Update curiosidade.py

--- curiosidade.py
+++ curiosidade.py
@@ -1,11 +1,17 @@
 import requests
+from datetime import datetime
 
 def main():
-    url = "http://numbersapi.com/today/date"
+    hoje = datetime.now()
+    mes = hoje.month
+    dia = hoje.day
+
+    url = f"http://numbersapi.com/{mes}/{dia}/date"
+
     try:
         resposta = requests.get(url)
         resposta.raise_for_status()
-        print("Curiosidade do dia:")
+        print("🧠 Curiosidade do dia:")
         print(resposta.text)
     except Exception as e:
         print("Erro ao buscar curiosidade:", e)
